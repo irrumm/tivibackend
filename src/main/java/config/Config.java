8cd7a55169d05be2aa8f899a490ded9c668bf8dd
irrumm <irrumm@ttu.ee>
2022-02-01 23:28:06 +0200

    gradle cleanup

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,6 +1,5 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
